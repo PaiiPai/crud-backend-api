a1e36c444a5b7ba2cb98bc6c6ea669a17cb2ee7b
Paiipai <paingfamily121@gmail.com>
2024-03-20 03:35:01 +0630

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const { pokemonRouter } = require("./routes/pokemonRouter");
+const mongoose = require("mongoose");
 
-// listen to port 3000
+// connect to MongoDB and listen to port 3000
 const port = 3000;
 const host = "localhost";
-app.listen(port, host, () => console.log(`Listening on port http://${host}:${port}/`));
+const user = encodeURIComponent("paipai");
+const password = encodeURIComponent("Choco95%");
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.cfktonb.mongodb.net/pokemondb`)
+.then(res => app.listen(port, host, () => console.log(`Listening on port http://${host}:${port}/`)))
+.catch(e => console.log(`Error: database connection failed | ${e}`));
 
 // use ejs
 app.set("view engine", "ejs");
