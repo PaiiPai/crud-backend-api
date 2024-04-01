2cbcc884b32f8601cc61ba26f830f52d90816a9a
Paiipai <paingfamily121@gmail.com>
2024-04-01 10:10:29 +0630

    Update pokemonController

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,11 +1,19 @@
 const Pokemon = require("../models/pokemon");
 
-const pokemonGet = (req, res) => {
-  res.status(200).json({
-    name: "Charmander",
-    type: "Fire",
-    ability: "Blaze",
-  });
+const pokemonGet = async (req, res) => {
+  try {
+    const pokemons = await Pokemon.find().sort({ id: "asc" });
+    console.log(pokemons);
+    res.status(200).json({
+      success: true,
+      pokemons,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: `Bad request: ${error}`,
+    });
+  }
 };
 
 const pokemonCreateGet = (req, res) => res.render("create");
@@ -15,13 +23,13 @@ const pokemonCreatePost = async (req, res) => {
   try {
     const pokemon = new Pokemon({ name, type, ability });
     const response = await pokemon.save();
-    
+
     res.status(200).json({
       success: true,
-    })
+    });
   } catch (error) {
     res.status(400).json({
-      error: `${error}`
+      error: `${error}`,
     });
   }
 };
